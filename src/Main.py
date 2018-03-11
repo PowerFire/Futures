bb9874e7c0f6c18f57f81da28d1fb31928de249d
Rody <rody0335@hotmail.com>
2018-03-12 00:12:40 +0800

    Study_Pandas

---
+++ src/Main.py
@@ -0,0 +1,12 @@
+# encoding=utf-8
+import pandas as pd
+
+df= pd.read_csv('Daily_2018_03_12.csv')
+print(type(df))
+#print(df.shape[0])
+print(df.index)
+#print(df.columns)
+#print( df['MTX    '])
+MTX=df[df['MTX    ']=='MTX    ']
+print(MTX.columns)
+print(MTX.iloc[:,4:6])
